161c4edc205d60dcc5f99c0974d4bf5b1e7f06b8
Kevin Amado <kamadorueda@gmail.com>
2022-10-27 23:25:56 -0600

    feat: add formatter
    - Run `yarn run format` to format files locally,
      and `yarn run check-format` to check that files are formatting
      on the CI
    
    Signed-off-by: Kevin Amado <kamadorueda@gmail.com>

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,8 @@
 import { App } from "./app";
 import { CalendarController } from "./controllers/calendar.controller";
 
-
 const port: number = 3001;
 
-const app = new App([new CalendarController()],port);
+const app = new App([new CalendarController()], port);
 
 app.listen();
-
-  
