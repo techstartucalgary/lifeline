eeed464211fea77fe3917719dad0e4e42f1abed8
Yunfan Yang <cloudy.young@outlook.com>
2022-11-01 21:13:40 -0600

    chore(app): add axios base url config
    Signed-off-by: Yunfan Yang <cloudy.young@outlook.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,27 @@
 import { Routes, Route } from "react-router-dom";
+import axios from 'axios';
+
 import Landing from "./pages/Landing";
 import Loading from "./pages/Loading";
 import Review from "./pages/Review";
 import ThankYou from "./components/ThankYou";
 import Upload from "./pages/Upload";
 import Workflow from "./pages/Workflow";
+
+
+const hostname = window.location.hostname;
+const port = window.location.port;
+
+if (hostname === "localhost" || hostname === "127.0.0.1" || port === "3000") {
+  axios.defaults.baseURL = `//${hostname}:3001`;
+  console.log("Running on Localhost", axios.defaults.baseURL);
+} else {
+  axios.defaults.baseURL = `https://api.${hostname}`;
+  console.log("Running on Production", axios.defaults.baseURL);
+}
+
+axios.defaults.withCredentials = true;
+axios.defaults.timeout = 30000;
 
 function App() {
   return (
