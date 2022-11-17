f4289f3332ac0a73de75cf38c84828d8dfe9f334
harsweet <hsbhangu0001@gmail.com>
2022-11-16 23:58:56 -0700

    Added support for file upload

--- server/app.py
+++ server/app.py
@@ -1,12 +1,16 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from handlers import calendarHandler
 
 app = FastAPI()
 
 @app.get("/calendarJson")
-def showCalendar():
+async def showCalendar():
     return calendarHandler.getCalendarJson()
+
+@app.post('/sendFile')
+async def sendOutlineFile(outlineFile: UploadFile=File(...)):
+    return {"The File that you just passed is ":outlineFile.filename}
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="127.0.0.1", port=8000)
