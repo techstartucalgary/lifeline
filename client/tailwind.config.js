136b4e2822f617e5a974d665c081f67366698165
Bradley <jizhe.li@ucalgary.cm>
2022-11-25 23:45:56 -0700

    The landing page is basically finished except the footer.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{tsx}"],
+  content: ["./**/src/**/*.{tsx, ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray': '#D9D9D9',
+        'pink': '#FF2480',
+        'getStarted': '#B7194A',
+        'learnMore': '#B7194A',
+        'divider': "ADADAD",
+        'start': '#B7194A'
+      },
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+      },
+      lineHeight: {
+        '12': '3rem',
+      }
+    },
   },
   plugins: [],
 };
