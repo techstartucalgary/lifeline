679b54443199961532654d4c6a0ed3e0fedba21a
Jaza <jaza-k@protonmail.com>
2022-12-01 15:27:25 -0700

    add some fast api middleware

--- server/app.py
+++ server/app.py
@@ -1,8 +1,22 @@
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
 from handlers import calendarHandler
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/calendarJson")
