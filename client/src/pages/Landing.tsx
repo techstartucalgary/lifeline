fcc2bcbf8452aa4e9ddfba90d1b4f8a254058c6a
Tim Macphail <70448914+tim-macphail@users.noreply.github.com>
2022-12-02 18:54:40 +0000

    fix: format client
    Signed-off-by: Tim Macphail <70448914+tim-macphail@users.noreply.github.com>

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -4,7 +4,9 @@ export default function About() {
   return (
     <>
       <main>
-        <h2 className="font-bold mt-10 text-emerald-500">This is the landing page</h2>
+        <h2 className="font-bold mt-10 text-emerald-500">
+          This is the landing page
+        </h2>
       </main>
 
       <nav>
