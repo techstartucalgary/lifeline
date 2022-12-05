37cf1969c00d00239441da2a175c515dbb4909ad
Tim Macphail <70448914+tim-macphail@users.noreply.github.com>
2022-12-05 19:26:33 +0000

    Revert "Merge pull request #21 from techstartucalgary/bradley/landing-page"
    This reverts commit f834676743e546441d73a3f487cf607ad7a419c8, reversing
    changes made to 30d22ac1a40ae960b36b486316bc3092be65e7de.

--- client/src/pages/Review.tsx
+++ client/src/pages/Review.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export default function Review() {
+export default function Home() {
   return (
     <>
       <main>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,22 +2,7 @@
 module.exports = {
   content: ["./**/src/**/*.{tsx, ts}"],
   theme: {
-    extend: {
-      colors: {
-        'gray': '#D9D9D9',
-        'pink': '#FF2480',
-        'getStarted': '#B7194A',
-        'learnMore': '#B7194A',
-        'divider': "ADADAD",
-        'start': '#B7194A'
-      },
-      fontFamily: {
-        poppins: "'Poppins', sans-serif",
-      },
-      lineHeight: {
-        '12': '3rem',
-      }
-    },
+    extend: {},
   },
   plugins: [
     require("@tailwindcss/typography"),
