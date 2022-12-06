576a6cc0774b01e7fbd163b979a8f94a981463e6
Tim Macphail <70448914+tim-macphail@users.noreply.github.com>
2022-12-06 00:47:14 +0000

    fix: run format

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -7,9 +7,7 @@ export default function About() {
         <h2 className="font-bold mt-10 text-emerald-500">
           This is the landing page
         </h2>
-        <span className="material-symbols-outlined">
-          expand_more
-        </span>
+        <span className="material-symbols-outlined">expand_more</span>
       </main>
 
       <nav>
