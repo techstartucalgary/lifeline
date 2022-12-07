e53ee8ff9a7695f5933982533f91e7d3e2cdf9b0
Bradley Li <66497679+BradleyLi@users.noreply.github.com>
2022-12-07 00:01:55 +0000

    formatted it a bit
    Signed-off-by: Bradley Li <66497679+BradleyLi@users.noreply.github.com>

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -11,13 +11,27 @@ export default function About() {
           </div>
           <div className="leading-relaxed font-poppins mt-10 w-11/12">
             <p className="text-xl font-normal non-italic">
-              Lifeline takes the stress out of managing important dates for your courses. Start saving more time and always keep track of your deadlines in just a few clicks.
-              <span className="block">Start for free today to make your scheduling a breeze.</span>
+              Lifeline takes the stress out of managing important dates for your
+              courses. Start saving more time and always keep track of your
+              deadlines in just a few clicks.
+              <span className="block">
+                Start for free today to make your scheduling a breeze.
+              </span>
             </p>
             <nav>
               <div className="mt-[60px]">
-                <Link to="/upload" className="rounded-full bg-getStarted text-white text-sm text-semibold p-3 px-6 pt-2 baseline">Get Started</Link>
-                <Link to="/workflow" className="text-learnMore text-sm text-semibold py-3.5 px-6 inline">Learn more</Link>
+                <Link
+                  to="/upload"
+                  className="rounded-full bg-getStarted text-white text-sm text-semibold p-3 px-6 pt-2 baseline"
+                >
+                  Get Started
+                </Link>
+                <Link
+                  to="/workflow"
+                  className="text-learnMore text-sm text-semibold py-3.5 px-6 inline"
+                >
+                  Learn more
+                </Link>
               </div>
             </nav>
           </div>
@@ -26,7 +40,6 @@ export default function About() {
         <div className="w-5/12">
           <img src="illustration1.svg" alt="illustration 1" />
         </div>
-
       </div>
 
       <div className="flex flex-row w-4/5 mt-36 mx-auto font-poppins">
@@ -38,7 +51,12 @@ export default function About() {
             <div className="flex flex-row">
               <img src="upload.svg" alt="upload" />
               <div className="ml-4">
-                <p className="text-lg block font-[600]">Upload your course outline<span className="block text-sm font-[400]">Up to 3 PDF documents are accepted</span></p>
+                <p className="text-lg block font-[600]">
+                  Upload your course outline
+                  <span className="block text-sm font-[400]">
+                    Up to 3 PDF documents are accepted
+                  </span>
+                </p>
               </div>
             </div>
           </div>
@@ -47,7 +65,13 @@ export default function About() {
             <div className="flex flex-row">
               <img src="edit.svg" alt="upload" />
               <div className="ml-4">
-                <p className="text-lg block font-[600]">Preview and edit<span className="block text-sm font-[400]">Lifeline extracts important information and generate your schedule, and get the chance to preview and make changes </span></p>
+                <p className="text-lg block font-[600]">
+                  Preview and edit
+                  <span className="block text-sm font-[400]">
+                    Lifeline extracts important information and generate your
+                    schedule, and get the chance to preview and make changes{" "}
+                  </span>
+                </p>
               </div>
             </div>
           </div>
@@ -56,36 +80,45 @@ export default function About() {
             <div className="flex flex-row">
               <img src="export.svg" alt="upload" />
               <div className="ml-4">
-                <p className="text-lg block font-[600]">Export final result into calendar<span className="block text-sm font-[400]">Download your ICS file and import it into your calendar app  </span></p>
+                <p className="text-lg block font-[600]">
+                  Export final result into calendar
+                  <span className="block text-sm font-[400]">
+                    Download your ICS file and import it into your calendar app{" "}
+                  </span>
+                </p>
               </div>
             </div>
           </div>
-
         </div>
       </div>
-
-
 
       <div className="flex flex-col items-center font-poppins mt-28">
         <p className="font-[600] text-2xl leading-12 text-center">
           Easily integrates into your favourite calendar
         </p>
         <p className="font-[400] text-base text-center w-3/5">
-          Lifeline generates your schedule in a convenient format, allowing you to comfortably share calendar information.
-          Import your schedule details into a digital calendar such as Microsoft Outlook, Google Calendar, or Apple Calendar.
+          Lifeline generates your schedule in a convenient format, allowing you
+          to comfortably share calendar information. Import your schedule
+          details into a digital calendar such as Microsoft Outlook, Google
+          Calendar, or Apple Calendar.
         </p>
         <img src="illustration10.svg" alt="illustration 10" className="mt-14" />
       </div>
-
 
       <div className="flex flex-col items-center mt-20 font-poppins">
         <p className="text-2xl font-[600] leading-12 tracking-tight">
           Ready to schedule with ease?
         </p>
         <p className="font-normal text-base w-2/5 text-center">
-          Start using Lifeline today to take control of your time and never worry about handling deadlines anymore.
+          Start using Lifeline today to take control of your time and never
+          worry about handling deadlines anymore.
         </p>
-        <Link to="/upload" className="rounded-full bg-getStarted text-white text-sm text-semibold p-3 px-6 pt-2 baseline my-8">Get Started</Link>
+        <Link
+          to="/upload"
+          className="rounded-full bg-getStarted text-white text-sm text-semibold p-3 px-6 pt-2 baseline my-8"
+        >
+          Get Started
+        </Link>
       </div>
 
       <div className=" bg-start rounded-full mx-auto text-white baseline">
@@ -93,7 +126,10 @@ export default function About() {
       </div>
 
       <div className="border-solid rounded-full bg-gray"></div>
-      <div className="text-sm text-normal">Designed with &hearts; in 2022<span className="block">All rights reserved.</span></div>
+      <div className="text-sm text-normal">
+        Designed with &hearts; in 2022
+        <span className="block">All rights reserved.</span>
+      </div>
     </>
   );
 }
