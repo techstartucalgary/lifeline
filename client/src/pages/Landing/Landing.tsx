a0ba30d5f26dcf48b310356a35f2c271af423344
Cloudy Young <yunfan.yang@agvisorpro.com>
2023-01-08 22:31:23 -0700

    Refactor introduction and get started section

--- client/src/pages/Landing/Landing.tsx
+++ client/src/pages/Landing/Landing.tsx
@@ -49,141 +49,165 @@ export default function Landing() {
   }, []);
 
   return (
-    <div className="font-display max-w-7xl mx-auto px-5 sm:px-14 md:px-16 lg:px-20">
-      <NavigationBar />
+    <>
+      <div className="font-display max-w-7xl mx-auto px-5 sm:px-14 md:px-16 lg:px-20">
+        <NavigationBar />
 
-      {/* Hero Section */}
-      <div
-        className={classnames(
-          "gap-3 grid",
-          "mt-16 sm:mt-20 md:mt-24 lg:mt-28",
-          "grid-cols-4 sm:grid-cols-8 md:grid-cols-8 lg:grid-cols-12"
-        )}
-      >
-        {/* Left section */}
+        {/* Hero section */}
         <div
           className={classnames(
-            "flex items-center",
-            "col-span-4 sm:col-span-8 md:col-span-8 lg:col-span-7",
-            "text-left sm:text-center lg:text-left",
+            "gap-3 grid",
+            "mt-32 sm:mt-20 md:mt-24 lg:mt-28",
+            "grid-cols-4 sm:grid-cols-8 md:grid-cols-8 lg:grid-cols-12"
           )}
         >
-          <div className="space-y-10 flex flex-col">
-            <div className={classnames("font-bold", "text-4xl sm:text-5xl md:text-6xl xl:text-6xl")}>
-              <div className="whitespace-nowrap">A better way to </div>
-              <div className={classnames("text-primary-50 title-highlight whitespace-nowrap", "pb-2 sm:pt-1")}>organize deadlines</div>
-            </div>
+          {/* Left section */}
+          <div
+            className={classnames(
+              "flex items-center",
+              "col-span-4 sm:col-span-8 md:col-span-8 lg:col-span-7",
+              "text-left sm:text-center lg:text-left",
+            )}
+          >
+            <div className="space-y-10 flex flex-col">
+              <div className={classnames("font-bold", "text-4xl sm:text-5xl md:text-6xl xl:text-6xl")}>
+                <div className="whitespace-nowrap">A better way to </div>
+                <div className={classnames("text-primary-50 title-highlight whitespace-nowrap", "pb-2 sm:pt-1")}>organize deadlines</div>
+              </div>
 
-            <div className={classnames("mr-8 sm:mr-0 lg:mr-20", "text-md sm:text-lg md:text-xl lg:text-xl")}>
-              Lifeline takes the stress out of managing important dates for your
-              courses. Start saving more time and always keep track of your
-              deadlines in just a few clicks.
-              Start for free today to make your scheduling a breeze.
-            </div>
+              <div className={classnames("mr-8 sm:mr-0 lg:mr-20", "text-md sm:text-lg md:text-xl lg:text-xl")}>
+                Lifeline takes the stress out of managing important dates for your
+                courses. Start saving more time and always keep track of your
+                deadlines in just a few clicks.
+                Start for free today to make your scheduling a breeze.
+              </div>
 
+              <div
+                className={classnames(
+                  "flex justify-start",
+                  "text-md sm:text-lg",
+                  "sm:justify-center lg:justify-start"
+                )}
+              >
+                <Button to="/upload" variant="filled" className="py-4 hidden sm:block mr-2">Get Started</Button>
+                <Button to="/workflow" variant="text" className="px-4 py-4 -ml-4 -mt-6 sm:ml-0 sm:mt-0">Learn more</Button>
+              </div>
+            </div>
+          </div>
+
+          {/* Right section */}
+          <div
+            className={classnames(
+              "mt-4 sm:mt-12 md:mt-12 lg:mt-0",
+              "col-span-4 sm:col-span-8 lg:col-span-5",
+              "flex justify-center lg:justify-start"
+            )}
+          >
+            <img src={illustration1} alt="an illustration of a girl scheduling her calendar" className="w-9/12 lg:w-full max-w-sm lg:max-w-none" />
+          </div>
+        </div>
+
+        {/* Flow section */}
+        <div
+          className={classnames(
+            "gap-3 grid",
+            "mt-16 sm:mt-20 md:mt-24 lg:mt-28",
+            "grid-cols-4 sm:grid-cols-8 md:grid-cols-8 lg:grid-cols-12",
+          )}
+        >
+          {/* Left section */}
+          <div
+            className={classnames(
+              "flex items-center",
+              "order-2 lg:order-1",
+              "mt-12 sm:mt-16 md:mt-20 lg:mt-0",
+              "mr-0 sm:mr-0 md:mr-0 lg:mr-10",
+              "col-span-4 sm:col-span-8 md:col-span-8 lg:col-span-6",
+              "flex justify-center lg:justify-start",
+            )}
+          >
+            <img src={illustration19} alt="illustration of two boys discussing ideas over calendar" className=" w-11/12 lg:w-full max-w-sm lg:max-w-none" />
+          </div>
+
+          {/* Right section */}
+          <div
+            className={classnames(
+              "flex items-center",
+              "order-1 lg:order-2",
+              "col-span-4 sm:col-span-8 md:col-span-8 lg:col-span-6",
+              "mx-0 sm:mx-16 md:mx-16 lg:mx-0"
+            )}
+          >
+            <div className="flex flex-col space-y-9">
+              <FlowStep title="Upload your course outline" description="Up to 3 PDF documents are accepted" icon="upload_file" />
+              <FlowStep title="Preview and edit" description="Lifeline extracts important information and generate your schedule, and get the chance to preview and make changes" icon="edit" />
+              <FlowStep title="Export your schedule" description="Download your schedule in a variety of formats, including PDF, iCal, and Google Calendar" icon="download" />
+            </div>
+          </div>
+        </div>
+
+        {/* Introduction section */}
+        <div
+          className={classnames(
+            "text-center",
+            "mt-16 sm:mt-20 md:mt-24 lg:mt-28",
+          )}
+        >
+          <div className="space-y-2">
+            <p className="text-md sm:text-lg md:text-xl lg:text-xl leading-6 font-semibold">
+              Easily integrates into your favourite calendar
+            </p>
+            <p className="text-sm sm:text-md md:text-lg lg:text-lg">
+              Lifeline generates your schedule in a convenient format, allowing you
+              to comfortably share calendar information. Import your schedule
+              details into a digital calendar such as Microsoft Outlook, Google
+              Calendar, or Apple Calendar.
+            </p>
+          </div>
+
+          <div className="mt-8 flex justify-center">
+            <img src={illustration10} alt="illustration of a girl finishing the last piece of a puzzle" className="w-8/12 max-w-sm" />
+          </div>
+        </div>
+
+        {/* Get Started section */}
+        <>
+          <div
+            className={classnames(
+              "text-center",
+              "mt-16 sm:mt-20 md:mt-24 lg:mt-28",
+            )}
+          >
+            <div className="space-y-2">
+              <p className="text-md sm:text-lg md:text-xl lg:text-xl leading-6 font-semibold">
+                Ready to schedule with ease?
+              </p>
+              <p className="text-sm sm:text-md md:text-lg lg:text-lg">
+                Start using Lifeline today to take control of your time and never
+                worry about handling deadlines anymore.
+              </p>
+            </div>
+          </div>
+
+          <div className="sticky bottom-0 sm:relative">
             <div
               className={classnames(
-                "flex justify-start",
+                "flex justify-center py-8",
                 "text-md sm:text-lg",
-                "sm:justify-center lg:justify-start"
               )}
             >
-              <Button to="/upload" variant="filled" className="py-4 hidden sm:block mr-2">Get Started</Button>
-              <Button to="/workflow" variant="text" className="px-4 py-4 -ml-4 -mt-6 sm:ml-0 sm:mt-0">Learn more</Button>
+              <Button to="/upload" variant="filled" className="py-4 shadow-xl sm:shadow-none">Get Started</Button>
             </div>
           </div>
-        </div>
-
-        {/* Right section */}
-        <div
-          className={classnames(
-            "mt-4 sm:mt-12 md:mt-12 lg:mt-0",
-            "col-span-4 sm:col-span-8 lg:col-span-5",
-            "flex justify-center lg:justify-start"
-          )}
-        >
-          <img src={illustration1} alt="an illustration of a girl scheduling her calendar" />
-        </div>
+        </>
       </div>
 
-      {/* Flow section */}
-      <div
-        className={classnames(
-          "gap-3 grid",
-          "mt-16 sm:mt-20 md:mt-24 lg:mt-28",
-          "grid-cols-4 sm:grid-cols-8 md:grid-cols-8 lg:grid-cols-12",
-        )}
-      >
-        {/* Left section */}
-        <div
-          className={classnames(
-            "flex items-center",
-            "order-2 lg:order-1",
-            "mt-12 sm:mt-16 md:mt-20 lg:mt-0",
-            "mr-0 sm:mr-0 md:mr-0 lg:mr-10",
-            "col-span-4 sm:col-span-8 md:col-span-8 lg:col-span-6",
-            "flex justify-center lg:justify-start",
-          )}
-        >
-          <img src={illustration19} alt="illustration of two boys discussing ideas over calendar" />
-        </div>
-
-        {/* Right section */}
-        <div
-          className={classnames(
-            "flex items-center",
-            "order-1 lg:order-2",
-            "col-span-4 sm:col-span-8 md:col-span-8 lg:col-span-6",
-            "mx-0 sm:mx-16 md:mx-16 lg:mx-0"
-          )}
-        >
-          <div className="flex flex-col space-y-9">
-            <FlowStep title="Upload your course outline" description="Up to 3 PDF documents are accepted" icon="upload_file" />
-            <FlowStep title="Preview and edit" description="Lifeline extracts important information and generate your schedule, and get the chance to preview and make changes" icon="edit" />
-            <FlowStep title="Export your schedule" description="Download your schedule in a variety of formats, including PDF, iCal, and Google Calendar" icon="download" />
-          </div>
-        </div>
-      </div>
-
-
-      <div className="flex flex-col items-center font-poppins mt-28">
-        <p className="font-[600] text-2xl leading-12 text-center">
-          Easily integrates into your favourite calendar
-        </p>
-        <p className="font-[400] text-base text-center w-3/5">
-          Lifeline generates your schedule in a convenient format, allowing you
-          to comfortably share calendar information. Import your schedule
-          details into a digital calendar such as Microsoft Outlook, Google
-          Calendar, or Apple Calendar.
-        </p>
-        <img src={illustration10} alt="illustration 10" className="mt-14" />
-      </div>
-
-      <div className="flex flex-col items-center mt-20 font-poppins">
-        <p className="text-2xl font-[600] leading-12 tracking-tight">
-          Ready to schedule with ease?
-        </p>
-        <p className="font-normal text-base w-2/5 text-center">
-          Start using Lifeline today to take control of your time and never
-          worry about handling deadlines anymore.
-        </p>
-        <Link
-          to="/upload"
-          className="rounded-full bg-getStarted text-white text-sm text-semibold p-3 px-6 pt-2 baseline my-8"
-        >
-          Get Started
-        </Link>
-      </div>
-
-      <div className=" bg-start rounded-full mx-auto text-white baseline">
-        Start &rarr;
-      </div>
-
-      <div className="border-solid rounded-full bg-gray"></div>
-      <div className="text-sm text-normal">
+      
+      <div className="bg-on-secondary-container text-white">
+        <div className="bg-sys-background rounded-b-3xl h-10"></div>
         Designed with &hearts; in 2022
         <span className="block">All rights reserved.</span>
       </div>
-    </div>
+    </>
   );
 }
