10d485cb6d6a3e18ecbe005a503e13b41ce97635
Bradley <jizhe.li@ucalgary.cm>
2023-01-18 23:13:27 -0700

    some changes

--- client/src/pages/Landing/Landing.tsx
+++ client/src/pages/Landing/Landing.tsx
@@ -190,6 +190,10 @@ export default function Landing() {
           </div>
         </div>
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 4d6400fc37c582d0d940de8ca4c310e91d0723e7
         {/* Get Started section */}
         <>
           <div
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,2 +1,6 @@
 // Please configure Tailwind in src/tailwind.config.js instead of this file.
+<<<<<<< HEAD
 module.exports = require("./src/tailwind.config");
+=======
+module.exports = require("./src/tailwind.config");
+>>>>>>> 4d6400fc37c582d0d940de8ca4c310e91d0723e7
