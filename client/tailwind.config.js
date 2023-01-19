be46955b96cb8e2f0b075ad836ff3c1b305cbded
Bradley <jizhe.li@ucalgary.cm>
2023-01-18 23:26:32 -0700

    commmited it

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,10 @@
 // Please configure Tailwind in src/tailwind.config.js instead of this file.
 <<<<<<< HEAD
+<<<<<<< HEAD
 module.exports = require("./src/tailwind.config");
+=======
+module.exports = require("./src/tailwind.config");
+>>>>>>> 4d6400fc37c582d0d940de8ca4c310e91d0723e7
 =======
 module.exports = require("./src/tailwind.config");
 >>>>>>> 4d6400fc37c582d0d940de8ca4c310e91d0723e7
