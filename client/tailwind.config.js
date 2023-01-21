23837c91bcad5aac899b18c315cc3c0c41f5defb
Kevin Amado <kamadorueda@gmail.com>
2023-01-21 05:15:39 -0700

    fix: production build
    - Similar to my last one, but using esm
    
    Signed-off-by: Kevin Amado <kamadorueda@gmail.com>

---
+++ client/tailwind.config.js
@@ -0,0 +1,7 @@
+// Please configure Tailwind in src/tailwind.config.js instead of this file.
+
+// eslint-disable-next-line no-global-assign, @typescript-eslint/no-var-requires
+require = require("esm")({});
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+module.exports = require("./src/tailwind.config.mjs").default;
