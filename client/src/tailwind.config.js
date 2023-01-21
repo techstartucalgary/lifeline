930b3f2c9b761e8fa5a8e5e16f01abb082a52b03
Cloudy Young <cloudy.young@outlook.com>
2023-01-21 16:23:14 -0700

    Add transition functions

--- client/src/tailwind.config.js
+++ client/src/tailwind.config.js
@@ -187,7 +187,15 @@ module.exports = {
       },
       fontSize: {
         "1.5xl": "1.36rem",
-      }
+      },
+      transitionTimingFunction: {
+        "standard": "cubic-bezier(0.2, 0.0, 0, 1.0)",
+        "standard-decelerate": "cubic-bezier(0, 0, 0, 1)",
+        "standard-accelerate": "cubic-bezier(0.3, 0, 1, 1)",
+        "emphasized": "cubic-bezier(0.2, 0.0, 0, 1.0)",
+        "emphasized-decelerate": "cubic-bezier(0.05, 0.7, 0.1, 1.0)",
+        "emphasized-accelerate": "cubic-bezier(0.3, 0.0, 0.8, 0.15)",
+      },
     },
   },
   plugins: [
