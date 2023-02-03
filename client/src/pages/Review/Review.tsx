b34bfb2d510228e4f1a22c8a36b8fd68cb96d320
Timothy Macphail <timothy.macphail@gmail.com>
2023-02-03 12:08:58 -0700

    Feat: different routes for courses in review page
    Signed-off-by: Timothy Macphail <timothy.macphail@gmail.com>

--- client/src/pages/Review/Review.tsx
+++ client/src/pages/Review/Review.tsx
@@ -1,36 +1,48 @@
+import { useParams, Link } from "react-router-dom";
 import Button from "../../components/Button";
 
-const Layout = () => {
+const courses = ["cpsc-413", "seng-513", "cpsc-481", "cpsc-449", "cpsc-457"];
+
+const Review = () => {
+  const { courseId } = useParams();
+  const format = (courseId: string | undefined) => courseId?.replace("-", " ").toUpperCase();
+
   return (
     <div className="flex h-screen">
-      <nav className="w-64 bg-gray-600 h-full fixed left-0 top-0">\
+      <nav className="w-64 bg-gray-600 h-full fixed left-0 top-0">
         <div className="flex flex-col h-full">
-          <Button variant="filled">CPSC 413</Button>
-          <Button variant="filled">CPSC 413</Button>
-          <Button variant="filled">CPSC 413</Button>
-          <Button variant="filled">CPSC 413</Button>
-          <Button variant="filled">CPSC 413</Button>
+          {courses.map((course) => (
+            <Link to={`/review/${course}`} key={course}>
+              <Button variant="filled">{format(course)}</Button>
+            </Link>
+          ))}
         </div>
       </nav>
       <div className="flex flex-col w-full ml-64">
-        <header className="bg-gray-300 w-full p-4">Bradley&apos;s app top bar</header>
-        <div className="flex h-full">
-          <div className="w-1/2 bg-yellow-400 p-4">
-            <ul>
-              <li>Assignment 1</li>
-              <li>Assignment 2</li>
-              <li>Assignment 3</li>
-              <li>Assignment 4</li>
-              <li>Assignment 5</li>
-            </ul>
-          </div>
-          <div className="w-1/2 bg-green-400 p-4">
-            <img src="./pdf.png" alt="the pdf viewer" />
-          </div>
-        </div>
+        {courseId ? (
+          <>
+            <header className="bg-gray-300 w-full p-4">COURSE NAME: {format(courseId)}</header>
+            <div className="flex h-full">
+              <div className="w-1/2 bg-yellow-400 p-4">
+                <ul>
+                  <li>Assignment 1</li>
+                  <li>Assignment 2</li>
+                  <li>Assignment 3</li>
+                  <li>Assignment 4</li>
+                  <li>Assignment 5</li>
+                </ul>
+              </div>
+              <div className="w-1/2 bg-green-400 p-4">
+                <img src="../pdf.png" alt="the pdf viewer" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <p>upload</p>
+        )}
       </div>
     </div>
   );
 };
 
-export default Layout;
+export default Review;
