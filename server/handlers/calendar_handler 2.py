5e2182421892696a40fe8e7a8b729d20b9d35c8a
alisha.nasir <alisha.nasir@ucalgary.ca>
2023-02-05 02:57:46 -0700

    Deleted unnecessary files

--- server/handlers/calendar_handler 2.py
+++
@@ -1,9 +0,0 @@
-"""package for working with json"""
-import json
-
-
-def get_calendar_json():
-    """retrieves the JSON for the Calendar"""
-    with open("data/calendar.json", encoding="utf8") as stream:
-        generated_calendar = json.load(stream)
-        return generated_calendar
