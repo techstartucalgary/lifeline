ee417309c27f192de53abcc896da76157080a7a4
Cloudy Young <cloudy.young@outlook.com>
2023-02-08 00:16:57 -0700

    Fix: Review page main section max-w and center align

--- client/src/pages/Review/Review.tsx
+++ client/src/pages/Review/Review.tsx
@@ -1,21 +1,39 @@
-import { useState } from "react";
+import { useState, useRef, useCallback, useLayoutEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import Button from "../../components/Button";
 import { classnames } from "../../Utilities";
 
 
 const Review = () => {
+  const navRef = useRef<HTMLDivElement>(null);
+  const mainRef = useRef<HTMLDivElement>(null);
+  const [mainMarginLeft, setMainMarginLeft] = useState(0);
+  useLayoutEffect(() => {
+    const onMainMarginLeft = () => {
+      if (mainRef.current && navRef.current) {
+        const marginLeft = mainRef.current.offsetLeft || 0;
+        const navWidth = navRef.current?.offsetWidth || 0;
+        setMainMarginLeft(Math.max(navWidth - marginLeft, 0));
+      }
+    };
+    onMainMarginLeft();
+    window.addEventListener("resize", onMainMarginLeft);
+    return () => window.removeEventListener("resize", onMainMarginLeft);
+  }, []);
+
   const courses = ["cpsc-413", "seng-513", "cpsc-481", "cpsc-449", "cpsc-457"];
   const [selectedTab, setSelectedTab] = useState(0);
   const { courseId } = useParams();
   const format = (courseId: string | undefined) => courseId?.replace("-", " ").toUpperCase();
 
   return (
-    <div className="flex flex-row h-screen">
+    <>
       <nav
         className={classnames(
           "w-full", "md:w-64", "bg-gray-600", "h-full", "fixed", "flex", "flex-col", "p-4", "items-center", "z-0", !courseId && "z-20"
-        )}>
+        )}
+        ref={navRef}
+      >
         <div className="flex flex-col h-full">
           {courses.map((course) => (
             <Button variant={courseId === course ? "filled" : "tonal"} to={`/review/${course}`} key={course}>
@@ -24,65 +42,67 @@ const Review = () => {
           ))}
         </div>
       </nav>
-      <main className="flex flex-col w-full ml-0 md:ml-64 z-10">
-        {courseId ? (
-          <>
-            <header className="bg-gray-300 w-full p-4 text-xl">
-              <Link to="/review">
-                <span
-                  className={classnames("material-icons", "md:hidden", "inline")}
-                  style={{ fontSize: "1.5rem", verticalAlign: "middle" }}
-                >
-                  arrow_back
-                </span>
+      <main className="max-w-7xl mx-auto w-full z-10" ref={mainRef}>
+        <div style={{ marginLeft: mainMarginLeft }}>
+          {courseId ? (
+            <>
+              <header className="bg-gray-300 w-full p-4 text-xl">
+                <Link to="/review">
+                  <span
+                    className={classnames("material-icons", "md:hidden", "inline")}
+                    style={{ fontSize: "1.5rem", verticalAlign: "middle" }}
+                  >
+                    arrow_back
+                  </span>
 
-              </Link>
-              <h1 className="text-4xl">
-                {format(courseId)}
-              </h1>
-              <h2 className="text-2xl">
-                Explorations in Information Security and Privacy
-              </h2>
-            </header>
-            <div className="flex flex-col md:flex-row">
-              <div className="w-full md:hidden flex flex-row">
-                <button
-                  className={`w-full bg-gray-300 p-2 ${selectedTab === 0 && "bg-red-500"}`}
-                  onClick={() => setSelectedTab(0)}
-                >
-                  Assessments
-                </button>
-                <button
-                  className={`w-full bg-gray-300 p-2 ${selectedTab === 1 && "bg-red-500"}`}
-                  onClick={() => setSelectedTab(1)}
-                >
-                  Document
-                </button>
-              </div>
-              <section className={classnames(
-                "w-full", "md:w-1/2", "border", "border-gray-300", "bg-gray-200", "p-4", "h-screen", selectedTab === 0 ? "" : "hidden md:block"
-              )}>
-                <ul>
-                  <li>Assessment 1</li>
-                  <li>Assessment 2</li>
-                  <li>Assessment 3</li>
-                  <li>Assessment 4</li>
-                  <li>Assessment 5</li>
-                </ul>
-              </section>
-              <section
-                className={classnames(
-                  "w-full", "md:w-1/2", "border", "border-gray-300", "bg-gray-600", "p-4", "h-screen", selectedTab === 1 ? "" : "hidden md:block"
+                </Link>
+                <h1 className="text-4xl">
+                  {format(courseId)}
+                </h1>
+                <h2 className="text-2xl">
+                  Explorations in Information Security and Privacy
+                </h2>
+              </header>
+              <div className="flex flex-col md:flex-row">
+                <div className="w-full md:hidden flex flex-row">
+                  <button
+                    className={`w-full bg-gray-300 p-2 ${selectedTab === 0 && "bg-red-500"}`}
+                    onClick={() => setSelectedTab(0)}
+                  >
+                    Assessments
+                  </button>
+                  <button
+                    className={`w-full bg-gray-300 p-2 ${selectedTab === 1 && "bg-red-500"}`}
+                    onClick={() => setSelectedTab(1)}
+                  >
+                    Document
+                  </button>
+                </div>
+                <section className={classnames(
+                  "w-full", "md:w-1/2", "border", "border-gray-300", "bg-gray-200", "p-4", "h-screen", selectedTab === 0 ? "" : "hidden md:block"
                 )}>
-                <img src="../pdf.png" alt="the pdf viewer" />
-              </section>
-            </div>
-          </>
-        ) : (
-          <p>upload</p>
-        )}
+                  <ul>
+                    <li>Assessment 1</li>
+                    <li>Assessment 2</li>
+                    <li>Assessment 3</li>
+                    <li>Assessment 4</li>
+                    <li>Assessment 5</li>
+                  </ul>
+                </section>
+                <section
+                  className={classnames(
+                    "w-full", "md:w-1/2", "border", "border-gray-300", "bg-gray-600", "p-4", "h-screen", selectedTab === 1 ? "" : "hidden md:block"
+                  )}>
+                  <img src="../pdf.png" alt="the pdf viewer" />
+                </section>
+              </div>
+            </>
+          ) : (
+            <p>upload</p>
+          )}
+        </div>
       </main>
-    </div >
+    </>
   );
 };
 
