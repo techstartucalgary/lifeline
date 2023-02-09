87a254af1d73b9dc00dc7209d2ce0ea7e20469b6
Timothy Macphail <timothy.macphail@gmail.com>
2023-02-09 16:35:16 -0700

    Feat: refactor test state
    Signed-off-by: Timothy Macphail <timothy.macphail@gmail.com>

--- client/src/pages/Review/Review.tsx
+++ client/src/pages/Review/Review.tsx
@@ -6,50 +6,52 @@ import { classnames } from "../../Utilities";
 import { Courses } from "../../logic/icsGen";
 import styles from "./Review.module.css";
 
+const testState: Courses = {
+  "PSYC 203": {
+    topic: "Psychology of Everyday Life",
+    assessments: [
+      {
+        name: "Identity Assignment",
+        date: "2021-10-21T18:00:00.000",
+        weight: "6%",
+      },
+      {
+        name: "Coping Profile Assignment",
+        date: "2021-10-29T18:00:00.000",
+        weight: "2%",
+      },
+      {
+        name: "Self-Reflection/Goal Setting Assignment",
+        date: "2021-12-07T18:00:00.000",
+        weight: "7%",
+      },
+      {
+        name: "Experiential-Learning/Article-Evaluation Course Component",
+        date: "2021-12-08T23:59:59.999",
+        weight: "4%",
+      },
+      {
+        name: "Exam 1",
+        date: "2021-10-14T00:00:00.000",
+        weight: "25%",
+      },
+      {
+        name: "Exam 2",
+        date: "2021-11-18T00:00:00.000",
+        weight: "25%",
+      },
+      {
+        name: "Exam 3/Final Exam",
+        date: "TBD",
+        weight: "31%",
+      },
+    ],
+  },
+};
+
 const Review = () => {
   const [selectedTab, setSelectedTab] = useState(0);
-  const [courses, setCourses] = useState<Courses>({
-    "PSYC 203": {
-      topic: "Psychology of Everyday Life",
-      assessments: [
-        {
-          name: "Identity Assignment",
-          date: "2021-10-21T18:00:00.000",
-          weight: "6%",
-        },
-        {
-          name: "Coping Profile Assignment",
-          date: "2021-10-29T18:00:00.000",
-          weight: "2%",
-        },
-        {
-          name: "Self-Reflection/Goal Setting Assignment",
-          date: "2021-12-07T18:00:00.000",
-          weight: "7%",
-        },
-        {
-          name: "Experiential-Learning/Article-Evaluation Course Component",
-          date: "2021-12-08T23:59:59.999",
-          weight: "4%",
-        },
-        {
-          name: "Exam 1",
-          date: "2021-10-14T00:00:00.000",
-          weight: "25%",
-        },
-        {
-          name: "Exam 2",
-          date: "2021-11-18T00:00:00.000",
-          weight: "25%",
-        },
-        {
-          name: "Exam 3/Final Exam",
-          date: "TBD",
-          weight: "31%",
-        },
-      ],
-    },
-  });
+  const [courses, setCourses] = useState<Courses>(testState);
   const displayFormat = (course: string) => course.replace(/-/g, " ").toUpperCase();
 
   let { courseId } = useParams();
