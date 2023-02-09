c39d5edb9892d8d15220da2ae488079a06dda308
Timothy Macphail <timothy.macphail@gmail.com>
2023-02-09 16:25:45 -0700

    FIx: handle None assessment names
    Signed-off-by: Timothy Macphail <timothy.macphail@gmail.com>

--- server/handlers/file_handler.py
+++ server/handlers/file_handler.py
@@ -77,7 +77,7 @@ def extract_assessments(table):
                 # Ignore dates that are too short to avoid false positives.
                 # The shortest a date can realistically be is 5 characters. e.g. Dec 1
                 continue
-            name = row[0].replace("\n",' ')  # use the text in the first cell as the name
+            name = row[0].replace("\n",' ') if row[0] else "Unknown"  # use the text in the first cell as the name
 
             weight = "unknown"
             for cell in row:
