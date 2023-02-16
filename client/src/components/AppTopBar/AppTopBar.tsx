c9e8716ccd69dda4e748d91e789fd59003d85641
Bradley Li <jizhe.li@ucalgary.ca>
2023-02-15 18:49:23 -0700

    fixed sm:text-2xl to md:text-2xl in AppTopBar.tsx
    Signed-off-by: Bradley Li <jizhe.li@ucalgary.ca>

--- client/src/components/AppTopBar/AppTopBar.tsx
+++ client/src/components/AppTopBar/AppTopBar.tsx
@@ -14,7 +14,7 @@ const AppTopBar = ({ courseId, description }: AppTopBarProps) => {
     <div className="flex flex-row flex-1 bg-sys-surface w-full mb-0 pt-8 pb-0">
       <div className="flex flex-row text-left flex-1 items-center ml-4">
         <div className="flex-1">
-          <h1 className="text-4xl mt-auto text-on-surface font-headline font-bold sm:text-2xl">
+          <h1 className="text-4xl mt-auto text-on-surface font-headline font-bold md:text-2xl">
             {format(courseId)}
           </h1>
           <h2 className="text-base font-normal text-outline">
