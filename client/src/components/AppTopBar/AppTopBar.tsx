c9f0e0459782736aa3e0e676410e8dbe700653e4
Bradley Li <jizhe.li@ucalgary.ca>
2023-02-16 12:06:21 -0700

    remove the unused import { useParams }
    Signed-off-by: Bradley Li <jizhe.li@ucalgary.ca>

--- client/src/components/AppTopBar/AppTopBar.tsx
+++ client/src/components/AppTopBar/AppTopBar.tsx
@@ -1,5 +1,4 @@
 import { HTMLAttributes } from "react";
-import { Link, useParams } from "react-router-dom";
 import { classnames } from "../../Utilities";
 import Button from "../../components/Button";
 
