65d9869d693b0578581f3c42e5b906b390b98983
Bradley Li <jizhe.li@ucalgary.ca>
2023-02-16 11:56:45 -0700

    fix the hidding issue with the two icons
    Signed-off-by: Bradley Li <jizhe.li@ucalgary.ca>

--- client/src/components/AppTopBar/AppTopBar.tsx
+++ client/src/components/AppTopBar/AppTopBar.tsx
@@ -11,6 +11,30 @@ interface AppTopBarProps extends HTMLAttributes<HTMLDivElement> {
 const AppTopBar = ({ courseId, description }: AppTopBarProps) => {
   const format = (courseId: string | undefined) => courseId?.replace("-", " ").toUpperCase();
 
+  // return (
+  //   <div className="flex flex-row flex-1 bg-sys-surface w-full mb-0 pt-8 pb-0">
+  //     <div className="flex flex-row text-left flex-1 items-center ml-4">
+  //       <div className="flex-1">
+  //         <h1 className="text-4xl mt-auto text-on-surface font-headline font-bold md:text-2xl">
+  //           {format(courseId)}
+  //         </h1>
+  //         <h2 className="text-base font-normal text-outline">
+  //           {description}
+  //         </h2>
+  //       </div>
+        
+  //       <Button>
+  //         <span className={classnames("material-symbols-outlined", "hidden md:inline")}>error</span>
+  //       </Button>
+  //       <Button>
+  //         <span className={classnames("material-symbols-outlined", "hidden md:inline ml-4 mr-3")}>delete</span>
+  //       </Button>
+  //       <span className="hidden sm:block">This is displayed in web view and hidden in mobile view.</span>
+        
+  //     </div>
+
+  //   </div>
+  // );
   return (
     <div className="flex flex-row flex-1 bg-sys-surface w-full mb-0 pt-8 pb-0">
       <div className="flex flex-row text-left flex-1 items-center ml-4">
@@ -18,20 +42,20 @@ const AppTopBar = ({ courseId, description }: AppTopBarProps) => {
           <h1 className="text-4xl mt-auto text-on-surface font-headline font-bold md:text-2xl">
             {format(courseId)}
           </h1>
-          <h2 className="text-base font-normal text-outline">
-            {description}
-          </h2>
+          <h2 className="text-base font-normal text-outline">{description}</h2>
         </div>
-        
-        <Button>
-          <span className={classnames("material-symbols-outlined", "md:block", "hidden")}>error</span>
-        </Button>
-        <Button>
-          <span className={classnames("material-symbols-outlined", "md:block ml-4 mr-3", "hidden")}>delete</span>
-        </Button>
-        
-      </div>
 
+        <Button className="hidden md:inline">
+          <span className={classnames("material-symbols-outlined")}>error</span>
+        </Button>
+        <Button className="hidden md:inline">
+          <span
+            className={classnames("material-symbols-outlined", "ml-4", "mr-3")}
+          >
+            delete
+          </span>
+        </Button>
+      </div>
     </div>
   );
 };
