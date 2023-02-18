ba119a8b28efb05e2b4311e16a3481ca156e7480
Cloudy Young <cloudy.young@outlook.com>
2023-02-18 04:07:34 -0700

    Create IconButton component

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -1,2 +1,6 @@
 export { default } from "./Button";
 export * from "./Button";
+export type { ButtonProps } from "./Button";
+export { default as IconButton } from "./IconButton";
+export * from "./IconButton";
+export type { IconButtonProps } from "./IconButton";
