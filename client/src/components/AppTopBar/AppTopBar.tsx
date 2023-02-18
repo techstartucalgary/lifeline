10867a00f12a0335928f4dc4e0ee63a5fbd14c43
Cloudy Young <cloudy.young@outlook.com>
2023-02-17 22:43:05 -0700

    Adjust flex box

--- client/src/components/AppTopBar/AppTopBar.tsx
+++ client/src/components/AppTopBar/AppTopBar.tsx
@@ -8,21 +8,21 @@ interface AppTopBarProps {
 
 const AppTopBar = ({ title, subtitle }: AppTopBarProps) => {
   return (
-    <div className={classnames("flex flex-row flex-1 bg-sys-surface", "px-4 md:px-8", "pt-48 md:pt-28", "pb-4 md:pb-2")}>
-      <div className="flex flex-row text-left flex-1 items-center">
-        <div className="flex-1 space-y-1">
+    <div className={classnames("flex flex-row bg-sys-surface", "px-4 md:px-8", "pt-48 md:pt-28", "pb-4 md:pb-2")}>
+      <div className="flex flex-row text-left grow items-center">
+        <div className="space-y-1 grow">
           <h1 className="text-4xl mt-auto text-on-surface font-headline font-bold md:text-2xl">
             {title}
           </h1>
           <h2 className="text-base font-normal text-outline">{subtitle}</h2>
         </div>
 
-        <Button className="hidden md:inline">
-          <span className={classnames("material-symbols-outlined")}>error</span>
+        <Button className="hidden md:inline text-on-surface-variant">
+          <span className="material-symbols-outlined">error</span>
         </Button>
-        <Button className="hidden md:inline">
+        <Button className="hidden md:inline text-on-surface-variant">
           <span
-            className={classnames("material-symbols-outlined", "ml-4", "mr-3")}
+            className="material-symbols-outlined"
           >
             delete
           </span>
