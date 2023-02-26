4e5ed75cabb61abf59e05714b8973b1b3da7bccb
Cloudy Young <cloudy.young@outlook.com>
2023-02-26 14:38:28 -0700

    Remove unused imports

--- client/src/pages/Landing/Landing.tsx
+++ client/src/pages/Landing/Landing.tsx
@@ -2,12 +2,10 @@ import { useEffect } from "react";
 
 import { classnames } from "../../Utilities";
 import { Button } from "../../components/Button";
-import NavigationBar from "../../components/NavigationBar";
 
 import wfh1 from "./wfh_1.svg";
 import wfh7 from "./wfh_7.svg";
 import wfh8 from "./wfh_8.svg";
-import blob1 from "./blob1.svg";
 import blob2 from "./blob2.svg";
 import blob3 from "./blob3.svg";
 import blob4 from "./blob4.svg";
