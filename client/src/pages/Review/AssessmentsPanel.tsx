1d4b4ba4f68b97067a857169f8971e5c74efd4a3
Cloudy Young <cloudy.young@outlook.com>
2023-02-27 03:15:35 -0700

    Update assessment list header

--- client/src/pages/Review/AssessmentsPanel.tsx
+++ client/src/pages/Review/AssessmentsPanel.tsx
@@ -3,6 +3,8 @@ import AssessmentCard from "../../components/AssessmentCard";
 import { Button } from "../../components/Button";
 import { Assessment } from "../../logic/icsGen";
 
+import divider from "./divider.svg";
+
 interface AssessmentPanelProp {
   assessments: Assessment[];
   onAssessmentClick(assessment: Assessment, index: number): void;
@@ -13,17 +15,21 @@ const AssessmentsPanel = ({
   onAssessmentClick,
 }: AssessmentPanelProp) => {
   return (
-    <>
+    <div className="group">
       <div
         className={classnames(
-          "hidden md:flex md:flex-row",
-          "w-full justify-between items-center mb-3"
+          "hidden md:flex md:flex-col relative",
+          "w-full justify-between items-center py-4 mb-3"
         )}
       >
-        <h1 className="text-sys-primary font-bold uppercase">Assessments</h1>
+        <img src={divider} className="w-full" />
         <Button
           variant="tonal"
-          className="px-3.5 py-0.5"
+          className={classnames(
+            "px-3.5 py-0.5 absolute top-1/2 -translate-y-1/2",
+            "transition-all ease-emphasized-decelerate",
+            "invisible opacity-0 group-hover:visible group-hover:opacity-100"
+          )}
           onClick={() =>
             onAssessmentClick(
               {
@@ -35,7 +41,9 @@ const AssessmentsPanel = ({
             )
           }
         >
-          <span className="material-symbols-outlined text-2xl text-primary">add</span>
+          <span className="material-symbols-outlined text-2xl text-primary">
+            add
+          </span>
         </Button>
       </div>
       <ul className="flex flex-col">
@@ -50,7 +58,7 @@ const AssessmentsPanel = ({
             </li>
           ))}
       </ul>
-    </>
+    </div>
   );
 };
 
