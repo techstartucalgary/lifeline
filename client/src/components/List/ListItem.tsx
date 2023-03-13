e0a18eede538f5669366698920819826700886db
Timothy Macphail <timothy.macphail@gmail.com>
2023-03-13 16:40:06 -0600

    Fix description truncation on mobile
    Signed-off-by: Timothy Macphail <timothy.macphail@gmail.com>

--- client/src/components/List/ListItem.tsx
+++ client/src/components/List/ListItem.tsx
@@ -10,37 +10,49 @@ interface ListItemProps extends Omit<ButtonProps, "icon"> {
   metadata?: ReactNode | string;
 }
 
-const ListItem = ({ title, supportingText, metadata, leadingIcon, trailingIcon, color = "primary", className, ...args }: ListItemProps) => {
+const ListItem = ({
+  title,
+  supportingText,
+  metadata,
+  leadingIcon,
+  trailingIcon,
+  color = "primary",
+  className,
+  ...args
+}: ListItemProps) => {
   return (
     <Button
       {...args}
       color={color}
       variant="text"
-      className={classnames(
-        "px-5.5 py-4.5 text-on-surface-variant rounded-none",
-        className
-      )}
+      className={classnames("px-5.5 py-4.5 text-on-surface-variant", className)}
       icon={
-        <span className={classnames(
-          "material-symbols-outlined text-3xl",
-          "flex justify-center items-center",
-        )}>{leadingIcon}</span>
+        <span
+          className={classnames(
+            "material-symbols-outlined text-3xl",
+            "flex justify-center items-center"
+          )}
+        >
+          {leadingIcon}
+        </span>
       }
     >
-      <div className="flex flex-col items-start justify-center ml-2 min-w-0">
-        <p className="font-medium text-lg align-middle leading-6 text-on-surface">
+      <div className="ml-2 truncate text-left">
+        <p className="font-medium text-lg leading-6 text-on-surface truncate">
           {title}
         </p>
-        <p className="truncate font-normal">
-          {supportingText}
-        </p>
+        <p className="font-normal truncate">{supportingText}</p>
       </div>
       <div className="ml-auto flex items-center justify-center text-base proportional-nums">
         <div className="font-normal">{metadata}</div>
-        <span className={classnames(
-          "material-symbols-outlined",
-          "flex justify-center items-center ml-1",
-        )}>{trailingIcon}</span>
+        <span
+          className={classnames(
+            "material-symbols-outlined",
+            "flex justify-center items-center ml-1"
+          )}
+        >
+          {trailingIcon}
+        </span>
       </div>
     </Button>
   );
