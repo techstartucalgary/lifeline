8e3e14679bce7731b4fb2676e8c6a5b90ba5cb17
Timothy Macphail <timothy.macphail@gmail.com>
2023-03-13 13:55:41 -0600

    Use react-modal bottom sheet
    Signed-off-by: Timothy Macphail <timothy.macphail@gmail.com>

--- client/src/components/MoreBottomSheet/MoreBottomSheet.tsx
+++ client/src/components/MoreBottomSheet/MoreBottomSheet.tsx
@@ -1,5 +1,5 @@
 import { motion, AnimatePresence } from "framer-motion";
-import { useEffect, useRef, useState } from "react";
+import Modal from "react-modal";
 
 import { IconButton } from "../Button";
 
@@ -9,68 +9,46 @@ type BottomSheetProps = {
 };
 
 const BottomSheet: React.FC<BottomSheetProps> = ({ isOpen, onClose }) => {
-  const containerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const handleOutsideClick = (event: MouseEvent) => {
-      event.stopPropagation();
-      event.preventDefault();
-      event.stopImmediatePropagation();
-      event.cancelBubble = true;
-      event.returnValue = false;
-
-      if (
-        containerRef.current &&
-        !containerRef.current.contains(event.target as Node)
-      ) {
-        console.log("closing bottom sheet");
-        onClose();
-      } else {
-        console.log("clicked inside bottom sheet");
-      }
-    };
-
-    if (isOpen) {
-      document.addEventListener("click", handleOutsideClick);
-    }
-
-    return () => {
-      document.removeEventListener("click", handleOutsideClick);
-    };
-  }, [onClose, isOpen]);
-
-  const variants = {
-    open: { y: 0 },
-    closed: { y: "100%" },
-  };
-
   return (
     <AnimatePresence>
       {isOpen && (
-        <motion.div
-          className="fixed bottom-0 left-0 right-0 bg-white rounded-t-lg shadow-lg overflow-hidden z-10"
-          initial="closed"
-          animate="open"
-          exit="closed"
-          variants={variants}
-          transition={{ duration: 0.2 }}
-          ref={containerRef}
+        <Modal
+          isOpen={true}
+          onRequestClose={onClose}
+          overlayClassName="fixed inset-0 bg-black bg-opacity-50 z-50"
+          className="fixed bottom-0 left-0 right-0"
+          closeTimeoutMS={200}
         >
-          <div className="rounded-t-2xl bg-surface-variant flex flex-row justify-center p-4 gap-1">
-            <span className="flex flex-col justify-start w-1/5 items-center text-center">
-              <IconButton icon="add" />
-              <label>Add assessment</label>
-            </span>
-            <span className="flex flex-col justify-start w-1/5 items-center text-center">
-              <IconButton icon="delete" />
-              <label>Delete course</label>
-            </span>
-            <span className="flex flex-col justify-start w-1/5 items-center text-center">
-              <IconButton icon="error" />
-              <label>Report</label>
-            </span>
-          </div>
-        </motion.div>
+          <motion.div
+            className="flex flex-col items-center  bg-red-100 rounded-t-lg p-4"
+            initial={{ y: "100%" }}
+            animate={{ y: "0%" }}
+            exit={{ y: "100%" }}
+            transition={{ type: "spring", stiffness: 300, damping: 30 }}
+            drag="y"
+            dragConstraints={{ top: 0, bottom: 0 }}
+            onDragEnd={(event, info) => {
+              if (info.offset.y > 100) {
+                onClose();
+              }
+            }}
+          >
+            <div className="rounded-t-2xl bg-surface-variant flex flex-row justify-center p-4 gap-1">
+              <span className="flex flex-col justify-start w-1/5 items-center text-center">
+                <IconButton icon="add" />
+                <label>Add assessment</label>
+              </span>
+              <span className="flex flex-col justify-start w-1/5 items-center text-center">
+                <IconButton icon="delete" />
+                <label>Delete course</label>
+              </span>
+              <span className="flex flex-col justify-start w-1/5 items-center text-center">
+                <IconButton icon="error" />
+                <label>Report</label>
+              </span>
+            </div>
+          </motion.div>
+        </Modal>
       )}
     </AnimatePresence>
   );
