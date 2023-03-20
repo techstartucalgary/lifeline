0ff3a63a986455829a2bd15c61d9686c6a9d30f4
Cloudy Young <cloudy.young@outlook.com>
2023-03-19 18:09:16 -0600

    Adjust assessment card

--- client/src/components/AssessmentCard/AssessmentCard.tsx
+++ client/src/components/AssessmentCard/AssessmentCard.tsx
@@ -1,3 +1,4 @@
+import { classnames } from "../../Utilities";
 import { Assessment } from "../../logic/icsGen";
 import { Button } from "../Button";
 
@@ -47,30 +48,36 @@ const AssessmentCard = ({
 }: AssessmentCardProps) => {
   return (
     <Button
-      className="w-full p-4 rounded-3xl bg-primary-95 hover:bg-primary-90"
+      className={classnames(
+        "w-full p-4 rounded-[20px] flex-col justify-center items-start font-normal text-base",
+        "bg-primary-95 hover:bg-primary-90"
+      )}
       onClick={() => onAssessmentClick(assessment)}
     >
-      <div className="relative h-12 w-12 flex justify-center items-center">
-        <img
-          src={blob}
-          className="absolute -z-10"
-          alt="assessment icon background"
-        />
-        <span className="material-symbols-outlined text-white text-xl">
-          {getIcon(assessment.name)}
-        </span>
+      <div className="flex flex-row space-x-2.5">
+        <div className="relative h-12 w-12 flex flex-row justify-center items-center">
+          <img
+            src={blob}
+            className="absolute -z-10"
+            alt="assessment icon background"
+          />
+          <span className="material-symbols-outlined text-white text-xl">
+            {getIcon(assessment.name)}
+          </span>
+        </div>
+        <div className="flex flex-col items-start text-left">
+          <h1 className="font-medium text-sys-on-primary-container">
+            {assessment.name}
+          </h1>
+          <h2 className="">{formatDate(assessment.date)}</h2>
+        </div>
       </div>
-      <div className="flex flex-col items-start text-left">
-        <h1 className="font-bold text-sys-on-primary-container">
-          {assessment.name}
-        </h1>
-        <h2 className="text-sm">{formatDate(assessment.date)}</h2>
-        <p className="text-sys-outline mt-2 text-sm font-normal">
-          Weight: {assessment.weight}
-          <br />
-          {assessment.notes}
-        </p>
-      </div>
+
+      <p className="text-on-primary-container">
+        Weight: {assessment.weight}
+        <br />
+        {assessment.notes}
+      </p>
     </Button>
   );
 };
