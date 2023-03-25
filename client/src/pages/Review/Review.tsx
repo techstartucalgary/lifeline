d30968280f72952fa3b29e9fc173df5d3d727c53
Cloudy Young <cloudy.young@outlook.com>
2023-03-25 03:07:47 -0600

    Add overflow property to html

--- client/src/pages/Review/Review.tsx
+++ client/src/pages/Review/Review.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { useEffectOnce } from "react-use";
+import { useEffectOnce, useUpdateEffect } from "react-use";
 
 import { useBreakpoint } from "../../Utilities";
 import { Dropzone } from "../../components/Dropzone";
@@ -23,17 +23,17 @@ const Review = () => {
     courseKey: string | undefined;
   }>();
 
-  const currentCourse = courses.find(
-    (course) => course.key === courseKeyURLParam
-  ) || null;
+  const currentCourse =
+    courses.find((course) => course.key === courseKeyURLParam) || null;
 
   const deleteCurrentCourse = () => {
     setCourses(
       coursesRef.current.filter((course) => course.key !== currentCourse?.key)
     );
-    coursesRef.current = coursesRef.current.filter(
-      (course) => course.key !== currentCourse?.key
-    ) || null;
+    coursesRef.current =
+      coursesRef.current.filter(
+        (course) => course.key !== currentCourse?.key
+      ) || null;
     navigate("/app");
   };
 
@@ -134,7 +134,7 @@ const Review = () => {
     coursesRef.current = parsedCourses;
   });
 
-  useEffect(() => {
+  useUpdateEffect(() => {
     localStorage.setItem("courses", JSON.stringify(courses));
   }, [courses]);
 
@@ -155,6 +155,9 @@ const Review = () => {
   };
 
   const isMobile = () => ["xs", "sm"].includes(breakpoint);
+
+  // Add `overflow-hidden` to html element when on app page
+  useEffectOnce(() => document.documentElement.classList.add("overflow-hidden"));
 
   return (
     <div className="overflow-hidden min-h-screen">
