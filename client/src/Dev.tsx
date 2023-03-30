1628b4db84576a7b4bd56fe948ffb704d44821e7
Timothy Macphail <timothy.macphail@gmail.com>
2023-03-30 16:55:42 -0600

    Follow headless modal docs
    Signed-off-by: Timothy Macphail <timothy.macphail@gmail.com>

--- client/src/Dev.tsx
+++ client/src/Dev.tsx
@@ -1,69 +1,56 @@
 import { Dialog, Transition } from "@headlessui/react";
-import { Fragment, useState } from "react";
+import { useState } from "react";
+import { useEffectOnce } from "react-use";
 
 import { IconButton } from "./components/Button";
 
-const BottomSheet = () => {
+const MyDialog = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const openModal = () => {
-    setIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsOpen(false);
-  };
+  useEffectOnce(() => {
+    setTimeout(() => setIsOpen(true), 1000);
+    console.log("Dialog opened");
+  });
 
   return (
     <>
-      {/* Button to open the bottom sheet */}
-      <button onClick={openModal}>Open Bottom Sheet</button>
-
-      {/* Bottom Sheet */}
-      <Transition show={isOpen} as={Fragment}>
+      <button onClick={() => setIsOpen(!isOpen)}>Open/close</button>
+      <Transition
+        show={isOpen}
+        enter="transition duration-100 ease-out"
+        enterFrom="transform scale-95 opacity-0"
+        enterTo="transform scale-100 opacity-100"
+        leave="transition duration-75 ease-out"
+        leaveFrom="transform scale-100 opacity-100"
+        leaveTo="transform scale-95 opacity-0"
+        // as={Fragment}
+      >
         <Dialog
-          as="div"
-          className="fixed inset-0 z-10 overflow-y-auto"
-          onClose={closeModal}
+          open={isOpen}
+          onClose={() => setIsOpen(false)}
+          className="relative z-50"
         >
-          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Dialog.Overlay className="fixed inset-0 transition-opacity bg-primary-30 bg-opacity-30" />
-            </Transition.Child>
+          <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
 
-            {/* Bottom Sheet Content */}
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          <div className="fixed inset-0 flex items-center justify-center p-4">
+            <Dialog.Panel
+              className="mx-auto max-w-sm fixed bottom-0 rounded-t-2xl bg-surface-variant flex flex-row justify-center p-4 gap-1"
+              role="dialog"
+              aria-modal="true"
             >
-              <div className="rounded-t-2xl bg-surface-variant flex flex-row justify-center p-4 gap-1 w-full">
-                <span className="flex flex-col justify-start w-1/5 items-center text-center">
-                  <IconButton icon="add" />
-                  <label>Add assessment</label>
-                </span>
-                <span className="flex flex-col justify-start w-1/5 items-center text-center">
-                  <IconButton icon="delete" />
-                  <label>Delete course</label>
-                </span>
-                <span className="flex flex-col justify-start w-1/5 items-center text-center">
-                  <IconButton icon="error" />
-                  <label>Report</label>
-                </span>
-              </div>
-            </Transition.Child>
+              <span className="flex flex-col justify-start w-1/5 items-center text-center">
+                <IconButton icon="add" />
+                <label>Add assessment</label>
+              </span>
+              <span className="flex flex-col justify-start w-1/5 items-center text-center">
+                <IconButton icon="delete" />
+                <label>Delete course</label>
+              </span>
+              <span className="flex flex-col justify-start w-1/5 items-center text-center">
+                <IconButton icon="error" />
+                <label>Report</label>
+              </span>
+            </Dialog.Panel>
           </div>
         </Dialog>
       </Transition>
@@ -71,4 +58,4 @@ const BottomSheet = () => {
   );
 };
 
-export default BottomSheet;
+export default MyDialog;
