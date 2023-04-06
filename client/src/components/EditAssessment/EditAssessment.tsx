ab8333e1c570523a9ce3146608bb326c7e0092bd
Cloudy Young <cloudy.young@outlook.com>
2023-04-06 14:35:19 -0600

    Update form

--- client/src/components/EditAssessment/EditAssessment.tsx
+++ client/src/components/EditAssessment/EditAssessment.tsx
@@ -1,6 +1,4 @@
-import { classnames } from "../../Utilities";
 import { Assessment } from "../../logic/icsGen";
-import { Button } from "../Button";
 
 import blob from "./blob-small.svg";
 
@@ -45,7 +43,7 @@ const EditAssessment = ({
           <input
             type="text"
             id="assessment_name"
-            className="block px-2.5 pb-2.5 pt-4 w-full bg-transparent rounded-xl peer"
+            className="block px-2.5 pb-2.5 pt-4 w-full bg-transparent rounded-xl peer appearance-none border-outline-variant caret-primary"
             placeholder=""
             value={assessment.name}
             onChange={(e) =>
@@ -54,7 +52,7 @@ const EditAssessment = ({
           />
           <label
             htmlFor="assessment_name"
-            className="absolute duration-300 transform -translate-y-4 scale-75 top-2 z-10 bg-white px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+            className="absolute duration-300 transform -translate-y-4 scale-75 top-2 z-10 bg-surface px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
           >
             Name
           </label>
@@ -64,7 +62,7 @@ const EditAssessment = ({
         <div className="w-14"></div>
         <input
           type="datetime-local"
-          className="rounded-xl w-full ml-2"
+          className="rounded-xl w-full ml-2 bg-transparent border-outline-variant"
           value={jsxInputFormat(assessment.date)}
           onChange={(e) =>
             onAssessmentChange({
@@ -80,7 +78,7 @@ const EditAssessment = ({
           <input
             type="text"
             id="assessment_weight"
-            className="block px-2.5 pb-2.5 pt-4 w-full bg-transparent rounded-xl peer"
+            className="block px-2.5 pb-2.5 pt-4 w-full bg-transparent rounded-xl peer border-outline-variant"
             placeholder=" "
             value={assessment.weight}
             onChange={handleWeightChange}
@@ -90,7 +88,7 @@ const EditAssessment = ({
           </div>
           <label
             htmlFor="assessment_weight"
-            className="absolute duration-300 transform -translate-y-4 scale-75 top-2 z-10 bg-white px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+            className="absolute duration-300 transform -translate-y-4 scale-75 top-2 z-10 bg-surface px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
           >
             Weight
           </label>
@@ -102,7 +100,7 @@ const EditAssessment = ({
           <input
             type="text"
             id="assessment_location"
-            className="block px-2.5 pb-2.5 pt-4 w-full bg-transparent rounded-xl peer"
+            className="block px-2.5 pb-2.5 pt-4 w-full bg-transparent rounded-xl peer border-outline-variant"
             placeholder=" "
             onChange={(e) =>
               onAssessmentChange({ ...assessment, location: e.target.value })
@@ -111,29 +109,18 @@ const EditAssessment = ({
           />
           <label
             htmlFor="assessment_location"
-            className="absolute duration-300 transform -translate-y-4 scale-75 top-2 z-10 bg-white px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+            className="absolute duration-300 rounded-full transform -translate-y-4 scale-75 top-2 z-10 bg-surface px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
           >
             Location
           </label>
         </div>
-      </div>
-      <div className="flex flex-row justify-center items-center">
-        <div className="w-14"></div>
-        <Button
-          type="button"
-          onClick={(e) => e.preventDefault()}
-          variant="filled"
-          className="text-3xl h-10 text-center flex flex-row justify-center items-center px-4"
-        >
-          <span className="material-symbols-outlined">add</span>
-        </Button>
       </div>
       <div className="flex flex-row w-full h-14">
         <BlobIcon icon="speaker_notes" />
         <div className="relative w-full ml-2">
           <textarea
             id="assessment_notes"
-            className="block px-2.5 pb-2.5 pt-4 w-full bg-transparent rounded-xl peer h-48"
+            className="block px-2.5 pb-2.5 pt-4 w-full bg-transparent rounded-xl peer h-48 border-outline-variant caret-primary"
             placeholder=" "
             onChange={(e) =>
               onAssessmentChange({ ...assessment, notes: e.target.value })
@@ -142,7 +129,7 @@ const EditAssessment = ({
           />
           <label
             htmlFor="assessment_notes"
-            className="absolute duration-300 transform -translate-y-4 scale-75 top-2 z-10 bg-white px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+            className="absolute duration-300 transform -translate-y-4 scale-75 top-2 z-10 bg-surface px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
           >
             Notes
           </label>
@@ -156,34 +143,15 @@ export default EditAssessment;
 
 const BlobIcon = ({ icon }: { icon: string }) => {
   return (
-    <div className="relative w-14">
+    <div className="relative w-14 flex flex-row justify-center items-center">
       <img
         src={blob}
         alt="icon background"
-        className={classnames(
-          "absolute",
-          "top-0",
-          // eslint-disable-next-line max-lines
-          "left-0",
-          "w-full",
-          "h-full"
-        )}
+        className="absolute top-0 left-0 w-full h-full"
       />
-      <span
-        className={classnames(
-          "material-symbols-outlined",
-          "absolute",
-          "top-1/2",
-          "left-1/2",
-          "transform",
-          "-translate-x-1/2",
-          "-translate-y-1/2",
-          "text-primary",
-          "text-xl"
-        )}
-      >
+      <div className="material-symbols-outlined text-primary text-xl">
         {icon}
-      </span>
+      </div>
     </div>
   );
 };
