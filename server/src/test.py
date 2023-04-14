02e0c740a25084d8c96ed0f158bb908a00eff995
Timothy Macphail <timothy.macphail@gmail.com>
2023-04-14 11:19:46 -0600

    Test environment vars again
    Signed-off-by: Timothy Macphail <timothy.macphail@gmail.com>

--- server/src/test.py
+++ server/src/test.py
@@ -5,5 +5,13 @@ from dotenv.main import load_dotenv
 
 load_dotenv()
 
-print("The connection string is:")
-print(os.getenv("CONNECTION_STRING"))
+connection_string = os.getenv("CONNECTION_STRING")
+
+if len(connection_string) > 0:
+    print("Connection string is not empty")
+else:
+    print("Connection string is empty")
+
+# scramble the connection string
+scrambled = connection_string[0:10] + "..." + connection_string[-10:]
+print(f"Connection string: {scrambled.upper()}")
