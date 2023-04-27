7a13e3e9565d1c645148c05c2bda48789775778c
Timothy Macphail <timothy.macphail@gmail.com>
2023-04-27 17:20:44 -0600

    Add sample outlines
    Signed-off-by: Timothy Macphail <timothy.macphail@gmail.com>

--- client/src/components/Dropzone/Dropzone.tsx
+++ client/src/components/Dropzone/Dropzone.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 
 import { classnames } from "../../Utilities";
@@ -37,10 +37,60 @@ const Dropzone = ({ onDrop, isLoading }: DropzoneProps) => {
       {!isLoading && (
         <>
           <span className="material-symbols-outlined text-5xl">add</span>
-          <p>
-            {isDragActive
-              ? "Drop the file(s) here..."
-              : "To get started, drag and drop your course outlines here or click \"Add course\""}
+          <p className="text-center">
+            {isDragActive ? (
+              <p>Drop the file(s) here...</p>
+            ) : (
+              <p>
+                To get started, drag and drop your course outlines here or click
+                &quot;Add course&quot;
+                <br />
+                If you just want to try the app, you can use these sample
+                outlines: <br />
+                <ul className="list-disc list-inside text-left">
+                  <li>
+                    <a
+                      className="text-blue-600 underline"
+                      href="https://github.com/techstartucalgary/lifeline/raw/main/server/test-data/ARHI201/ARHI201.pdf"
+                    >
+                      Introduction to Art History 1
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-blue-600 underline"
+                      href="https://github.com/techstartucalgary/lifeline/raw/main/server/test-data/CPSC331/CPSC331.pdf"
+                    >
+                      Data Structures, Algorithms, and Their Analysis
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-blue-600 underline"
+                      href="https://github.com/techstartucalgary/lifeline/raw/main/server/test-data/CPSC457/CPSC457.pdf"
+                    >
+                      Principles of Operating Systems
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-blue-600 underline"
+                      href="https://github.com/techstartucalgary/lifeline/raw/main/server/test-data/INTE505.02/INTE505.02.pdf"
+                    >
+                      Science Internship Fall Work Term Placement
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-blue-600 underline"
+                      href="https://github.com/techstartucalgary/lifeline/raw/main/server/test-data/PSYC203/PSYC203.pdf"
+                    >
+                      Psychology of Everyday Life
+                    </a>
+                  </li>
+                </ul>
+              </p>
+            )}
           </p>
         </>
       )}
