f3351d8e743ee573e886c4fb611d4c4de250c064
tim-macphail <timothy.macphail@gmail.com>
2024-09-30 11:08:13 -0600

    remove mongodb interaction since the cluster is non-existent now (I think)
    Signed-off-by: tim-macphail <timothy.macphail@gmail.com>

--- server/src/handlers/db_handler.py
+++ server/src/handlers/db_handler.py
@@ -1,37 +1,37 @@
-"""Handler for the database"""
-import json
+# """Handler for the database"""
+# import json
 
-from dotenv.main import load_dotenv
-from pymongo import MongoClient
+# from dotenv.main import load_dotenv
+# from pymongo import MongoClient
 
-# Getting the connection string
-load_dotenv()
-# pylint ignore next line naming and line length
-# pylint: disable=C0103, C0301
-connection_string = "mongodb+srv://lifelineteamdb:mypassword1@cluster0.ib2qbgg.mongodb.net/?retryWrites=true&w=majority"
+# # Getting the connection string
+# load_dotenv()
+# # pylint ignore next line naming and line length
+# # pylint: disable=C0103, C0301
+# connection_string = "mongodb+srv://lifelineteamdb:mypassword1@cluster0.ib2qbgg.mongodb.net/?retryWrites=true&w=majority"
 
-# Defining a MongoDB client instance
-client = MongoClient(connection_string)
+# # Defining a MongoDB client instance
+# client = MongoClient(connection_string)
 
-# DB name
-db = client["database1"]
+# # DB name
+# db = client["database1"]
 
-# Referencing the collection
-courses = db["courses"]
-
-
-def insert_course_into_db(sha: str, course_str: str):
-    """Inserts a course into the database"""
-    course = {"sha": sha, "course_str": course_str}
-    result = courses.insert_one(course)
-    print("Course inserted with ID:", result.inserted_id)
+# # Referencing the collection
+# courses = db["courses"]
 
 
-def query_course_from_db(sha: str):
-    """Queries the database for a course with the given sha"""
-    course = courses.find_one({"sha": sha})
-    if course:
-        print("Found a cached course with ID:", course["_id"])
-        return json.loads(course["course_str"])
-    print("Didn't find a cached course")
-    return None
+# def insert_course_into_db(sha: str, course_str: str):
+#     """Inserts a course into the database"""
+#     course = {"sha": sha, "course_str": course_str}
+#     result = courses.insert_one(course)
+#     print("Course inserted with ID:", result.inserted_id)
+
+
+# def query_course_from_db(sha: str):
+#     """Queries the database for a course with the given sha"""
+#     course = courses.find_one({"sha": sha})
+#     if course:
+#         print("Found a cached course with ID:", course["_id"])
+#         return json.loads(course["course_str"])
+#     print("Didn't find a cached course")
+#     return None
